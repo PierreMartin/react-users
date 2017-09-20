2cfd9b2deece073921011e1e2961aaf2092cab03
Pierre Martin <pierre.martin.prestataire@sfr.com>
2017-09-20 13:59:29 +0200

    chat - css message content

--- app/components/chat/componantsChat/chatMessages.js
+++ app/components/chat/componantsChat/chatMessages.js
@@ -5,10 +5,38 @@ import styles from '../css/style';
 const cx = classNames.bind(styles);
 
 
+const renderItemsMessage = () => {
+	let nodeItemsMessages = [];
+	const numberItems = 20;
+
+	for (var i = 1; i <= numberItems; i++) {
+		nodeItemsMessages.push(
+			<div className={cx('message-content', 'received')} key={i}>
+				<div className={cx('message-avatar')}></div>
+				<div className={cx('message-text')}>Lorem ipsum dolor sit amet.</div>
+			</div>
+		);
+	}
+
+	return nodeItemsMessages;
+};
+
 const ChatMessages = ({ test }) => {
 	return (
-		<div className={cx('test')}>
-			{test}
+		<div className={cx('chatbox-messages-container')}  >
+
+			{renderItemsMessage()}
+
+			<div className={cx('message-content', 'sent')} >
+				<div className={cx('message-text')}>Hello !</div>
+				<div className={cx('message-avatar')}></div>
+			</div>
+
+			<div className={cx('message-content', 'sent')} >
+				<div className={cx('message-text')}>Hello ! Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</div>
+				<div className={cx('message-avatar')}></div>
+			</div>
+
 		</div>
 	);
 };
